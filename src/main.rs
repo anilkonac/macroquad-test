07ebe01f2466fdab790026e3b357bf0e69d2dbaa
Anıl Konaç <anil.konac@gmail.com>
2024-01-28 07:11:40 +0300

    Draw the player triangle using rotation matrix

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,13 @@ async fn main() {
     let mut speed = Vec2::ZERO;
     let mut pos = vec2(screen_width() / 2.0, screen_height() / 2.0);
     let mut direction = Vec2::ZERO;
+    let mut rotation = 0.0;
 
     loop {
         clear_background(BLACK);
 
         // Handle input
-        get_input_direction(&mut direction);
+        get_input_direction(&mut direction, &mut rotation);
 
         // Update speed and position
         let dt = get_frame_time();
@@ -24,7 +25,7 @@ async fn main() {
         pos += speed * dt;
 
         // Draw
-        draw_player(&pos);
+        draw_player(pos, rotation);
         draw_text_speed(&speed);
         // draw_text_fps();
 
@@ -32,14 +33,14 @@ async fn main() {
     }
 }
 
-fn get_input_direction(direction: &mut Vec2) {
+fn get_input_direction(direction: &mut Vec2, rotation: &mut f32) {
     *direction = Vec2::ZERO;
 
     if is_key_down(KeyCode::Right) || is_key_down(KeyCode::D) {
-        direction.x += 1.0;
+        *rotation += 1.0;
     }
     if is_key_down(KeyCode::Left) || is_key_down(KeyCode::A) {
-        direction.x -= 1.0;
+        *rotation -= 1.0;
     }
     if is_key_down(KeyCode::Up) || is_key_down(KeyCode::W) {
         direction.y -= 1.0;
@@ -49,13 +50,25 @@ fn get_input_direction(direction: &mut Vec2) {
     }
 }
 
-fn draw_player(pos: &Vec2) {
+fn draw_player(pos: Vec2, rot: f32) {
     static RADIUS: f32 = 20.0;
     static R_COS30: f32 = RADIUS * 0.86602540378;
     static R_SIN30: f32 = RADIUS * 0.5;
-    let v1 = vec2(pos.x, pos.y - RADIUS);
-    let v2 = vec2(pos.x - R_COS30, pos.y + R_SIN30);
-    let v3 = vec2(pos.x + R_COS30, pos.y + R_SIN30);
+    static V1: Vec2 = vec2(0.0, -RADIUS);
+    static V2: Vec2 = vec2(-R_COS30, R_SIN30);
+    static V3: Vec2 = vec2(R_COS30, R_SIN30);
+
+    let rot_rad = rot.to_radians();
+    let rot_rad_cos = rot_rad.cos();
+    let rot_rad_sin = rot_rad.sin();
+    let rotation_matrix = mat2(
+        vec2(rot_rad_cos, rot_rad_sin),
+        vec2(-rot_rad_sin, rot_rad_cos),
+    );
+
+    let v1 = rotation_matrix.mul_vec2(V1) + pos;
+    let v2 = rotation_matrix.mul_vec2(V2) + pos;
+    let v3 = rotation_matrix.mul_vec2(V3) + pos;
     draw_triangle_lines(v1, v2, v3, 2.0, WHITE);
 }
 
