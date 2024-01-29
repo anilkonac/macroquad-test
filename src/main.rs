135e541d49e917cd8dfdc97ff27c41b5095329ac
Anıl Konaç <anil.konac@gmail.com>
2024-01-29 22:15:37 +0300

    Rotate the player ship with angular acceleration

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,61 @@
+use std::f32::consts::{FRAC_PI_2, PI};
+
 use macroquad::prelude::*;
 
 #[macroquad::main("SimpleGame")]
 async fn main() {
-    const ACCELERATION: f32 = 250.0;
-    const MAX_VELOCITY: f32 = 300.0;
+    const ACCELERATION: f32 = 100.0;
+    const ACCELERATION_ANGULAR: f32 = 100.0;
+    const MAX_VELOCITY: f32 = 200.0;
+    const MAX_VELOCITY_ANGULAR: f32 = 200.0;
 
     let mut speed = Vec2::ZERO;
-    let mut pos = vec2(screen_width() / 2.0, screen_height() / 2.0);
-    let mut direction = Vec2::ZERO;
+    let mut speed_angular = 0.0;
+    let mut position = vec2(screen_width() / 2.0, screen_height() / 2.0);
+    let mut input_direction = Vec2::ZERO;
     let mut rotation = 0.0;
 
     loop {
         clear_background(BLACK);
 
         // Handle input
-        get_input_direction(&mut direction, &mut rotation);
+        get_input_direction(&mut input_direction);
 
         // Update speed and position
         let dt = get_frame_time();
-        if direction != Vec2::ZERO {
-            speed += direction.normalize() * ACCELERATION * dt;
+        if input_direction != Vec2::ZERO {
+            speed_angular += input_direction.y * ACCELERATION_ANGULAR * dt;
+            speed_angular = speed_angular.clamp(-MAX_VELOCITY_ANGULAR, MAX_VELOCITY_ANGULAR);
+            let direction = Vec2::from_angle(-FRAC_PI_2 + f32::to_radians(rotation));
+            speed += direction * input_direction.x * ACCELERATION * dt;
             speed = speed.clamp_length_max(MAX_VELOCITY);
         }
-        pos += speed * dt;
+        position += speed * dt;
+        rotation += speed_angular * dt;
 
         // Draw
-        draw_player(pos, rotation);
-        draw_text_speed(&speed);
+        draw_player(position, rotation);
+        // draw_text_speed(&speed);
         // draw_text_fps();
 
         next_frame().await
     }
 }
 
-fn get_input_direction(direction: &mut Vec2, rotation: &mut f32) {
+fn get_input_direction(direction: &mut Vec2) {
     *direction = Vec2::ZERO;
 
     if is_key_down(KeyCode::Right) || is_key_down(KeyCode::D) {
-        *rotation += 1.0;
+        direction.y += 1.0;
     }
     if is_key_down(KeyCode::Left) || is_key_down(KeyCode::A) {
-        *rotation -= 1.0;
-    }
-    if is_key_down(KeyCode::Up) || is_key_down(KeyCode::W) {
         direction.y -= 1.0;
     }
+    if is_key_down(KeyCode::Up) || is_key_down(KeyCode::W) {
+        direction.x = 1.0;
+    }
     if is_key_down(KeyCode::Down) || is_key_down(KeyCode::S) {
-        direction.y += 1.0;
+        direction.x -= 1.0;
     }
 }
 
