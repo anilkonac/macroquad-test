3c245f118c5b424b118ec941e6f0ed7817b09b01
Anıl Konaç <anil.konac@gmail.com>
2024-02-06 00:50:08 +0300

    Remove unnecessary calculations for laser
    Since the laser direction does not change, it does not need to be updated every frame.
    
    Avoid rotation vector calculation by changing the vertices of the laser.

--- src/laser/laser.rs
+++ src/laser/laser.rs
@@ -6,14 +6,14 @@ use macroquad_test::draw_line_w_rot;
 use crate::ship::SHIP_RADIUS;
 
 const LASER_VELOCITY: f32 = 200.0;
-const V_LAS_TOP: Vec2 = vec2(0.0, -2.0);
-const V_LAS_BTTM: Vec2 = vec2(0.0, 2.0);
+const V_LAS_1: Vec2 = vec2(-2.0, 0.0);
+const V_LAS_2: Vec2 = vec2(2.0, 0.0);
 
 #[derive(Default, Clone)]
 pub struct Laser {
     position: Vec2,
     speed: Vec2,
-    rotation_rad: f32,
+    direction: Vec2,
     pub active: bool,
 }
 
@@ -21,18 +21,17 @@ impl Laser {
     pub fn init(&mut self, ship_pos: Vec2, ship_rot_rad: f32, ship_speed: Vec2) {
         self.active = true;
         self.position = ship_pos + Vec2::from_angle(ship_rot_rad).rotate(vec2(0.0, -SHIP_RADIUS));
-        self.rotation_rad = ship_rot_rad;
+        self.direction = Vec2::from_angle(-FRAC_PI_2 + ship_rot_rad);
         self.speed = ship_speed;
     }
 
+    #[inline]
     pub fn update(&mut self, dt: f32) {
-        let direction = Vec2::from_angle(-FRAC_PI_2 + self.rotation_rad);
-        self.position += (self.speed + direction * LASER_VELOCITY) * dt;
+        self.position += (self.speed + self.direction * LASER_VELOCITY) * dt;
     }
 
     #[inline]
     pub fn draw(&self) {
-        let rot_vec = Vec2::from_angle(self.rotation_rad);
-        draw_line_w_rot(rot_vec, self.position, V_LAS_TOP, V_LAS_BTTM, 1.0, RED)
+        draw_line_w_rot(self.direction, self.position, V_LAS_1, V_LAS_2, 1.0, RED)
     }
 }
