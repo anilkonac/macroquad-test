713a0f8fa5169bb7ce354b65273b5216768328d5
Anıl Konaç <anil.konac@gmail.com>
2024-04-19 00:01:38 +0300

    wip

--- src/teleport.rs
+++ src/teleport.rs
@@ -8,31 +8,18 @@ pub trait Teleport {
     fn teleport(&mut self, screen_size: Vec2, offset: f32) {
         let pos = *self.position_mut();
 
-        let off_screen_left = pos.x < -offset;
-        let off_screen_right = pos.x > screen_size.x + offset;
-        let off_screen_up = pos.y < -offset;
-        let off_screen_down = pos.y > screen_size.y + offset;
-        if !off_screen_left && !off_screen_right && !off_screen_up && !off_screen_down {
-            return;
+        let lerp_pos_y = || lerp(pos.y, screen_size.y - pos.y, self.speed_dir().abs().y / 1.0);
+        if pos.x < -offset {
+            *self.position_mut() = vec2(screen_size.x + offset, lerp_pos_y());
+        } else if pos.x > screen_size.x + offset {
+            *self.position_mut() = vec2(-offset, lerp_pos_y());
         }
 
-        let abs_speed_direction = self.speed_dir().abs();
-        let pos = self.position_mut();
-
-        let lerp_mult = abs_speed_direction.y / 1.0;
-        let pos_y_lerp = || lerp(pos.y, screen_size.y - pos.y, lerp_mult);
-        if off_screen_left {
-            *pos = vec2(screen_size.x + offset, pos_y_lerp());
-        } else if off_screen_right {
-            *pos = vec2(-offset, pos_y_lerp());
-        }
-
-        let lerp_mult = abs_speed_direction.x / 1.0;
-        let pos_x_lerp = || lerp(pos.x, screen_size.x - pos.x, lerp_mult);
-        if off_screen_up {
-            *pos = vec2(pos_x_lerp(), screen_size.y + offset);
-        } else if off_screen_down {
-            *pos = vec2(pos_x_lerp(), -offset);
+        let lerp_pos_x = || lerp(pos.x, screen_size.x - pos.x, self.speed_dir().abs().x / 1.0);
+        if pos.y < -offset {
+            *self.position_mut() = vec2(lerp_pos_x(), screen_size.y + offset);
+        } else if pos.y > screen_size.y + offset {
+            *self.position_mut() = vec2(lerp_pos_x(), -offset);
         }
     }
 }
